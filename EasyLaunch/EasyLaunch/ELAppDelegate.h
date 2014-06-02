1c72bb50f6ee2a953e675a241569246b1e81bc91
Ilton Garcia <ilton_unb@hotmail.com>
2014-06-02 15:11:59 -0300

    initial commit

---
+++ EasyLaunch/EasyLaunch/ELAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ELAppDelegate.h
+//  EasyLaunch
+//
+//  Created by Ilton  Garcia on 02/06/14.
+//  Copyright (c) 2014 EasyLaunches. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ELAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
