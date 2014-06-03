10d7fdade0371673190e4f90e25f4760e27e8c6b
Ilton Garcia <ilton_unb@hotmail.com>
2014-06-03 09:05:05 -0300

    initial commit with storyboard

---
+++ EasyLaunches/EasyLaunches/ELViewController.m
@@ -0,0 +1,29 @@
+//
+//  ELViewController.m
+//  EasyLaunches
+//
+//  Created by Ilton  Garcia on 02/06/14.
+//  Copyright (c) 2014 EasyLaunches. All rights reserved.
+//
+
+#import "ELViewController.h"
+
+@interface ELViewController ()
+
+@end
+
+@implementation ELViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ EasyLaunches/EasyLaunches/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EasyLaunches
+//
+//  Created by Ilton  Garcia on 02/06/14.
+//  Copyright (c) 2014 EasyLaunches. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ELAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ELAppDelegate class]));
+    }
+}
