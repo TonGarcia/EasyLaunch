651283fd1c6444afa325cc0fb7ec48961c369882
Ilton Garcia <ilton_unb@hotmail.com>
2014-06-20 20:27:33 -0300

    POST request added

--- EasyLaunches/EasyLaunches/ELWSConnector.h
+++ EasyLaunches/EasyLaunches/ELWSConnector.h
@@ -9,5 +9,5 @@
 #import <Foundation/Foundation.h>
 
 @interface ELWSConnector : NSObject
-    + (NSString*) postTransaction;
+    + (NSString*) postTransaction:(NSString*)json;
 @end
--- EasyLaunches/EasyLaunches/ELWSConnector.m
+++ EasyLaunches/EasyLaunches/ELWSConnector.m
@@ -9,8 +9,37 @@
 #import "ELWSConnector.h"
 
 @implementation ELWSConnector
-+ (NSString*) postTransaction
++ (NSString*) postTransaction:(NSString*)json
 {
-        return @"";
+    NSString* method = @"http://";
+    NSString* domain = @"192.168.0.6:3000";
+    NSString* route = @"/api/transactions";
+    NSString* url = [
+                     NSString stringWithFormat:
+                        @"%@%@%@",
+                        method,
+                        domain,
+                        route
+                    ];
+    
+    
+    NSURL *URL = [NSURL URLWithString:url];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:URL];
+    // Set request type
+    request.HTTPMethod = @"POST";
+    
+    // Set params to be sent to the server
+    NSString *params = json;
+    // Encoding type
+    NSData *data = [params dataUsingEncoding:NSUTF8StringEncoding];
+    // Add values and contenttype to the http header
+    [request addValue:@"8bit" forHTTPHeaderField:@"Content-Transfer-Encoding"];
+    [request addValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    [request addValue:[NSString stringWithFormat:@"%lu", (unsigned long)[data length]] forHTTPHeaderField:@"Content-Length"];
+    [request setHTTPBody:data];
+    
+    // Send the request
+    [NSURLConnection connectionWithRequest:request delegate:self];
+    return @"";
 }
 @end
