bef92ccce91f8379f623fe1faadb26a2e8e33aa9
BryanFernandes <bryan.h.fernandes@hotmail.com>
2014-06-25 04:31:28 -0300

    * TableView DataSource and Delegate implemented. * Minor bugs fixed.

--- EasyLaunches/EasyLaunches/ELSendToCloudViewController.m
+++ EasyLaunches/EasyLaunches/ELSendToCloudViewController.m
@@ -15,6 +15,9 @@
 @end
 
 @implementation ELSendToCloudViewController
+{
+    NSArray *test;
+}
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -29,12 +32,32 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    test = [NSArray arrayWithObjects:@"Egg Benedict", @"Mushroom Risotto", @"Full Breakfast", @"Hamburger", @"Ham and Egg Sandwich", @"Creme Brelee", @"White Chocolate Donut", @"Starbucks Coffee", @"Vegetable Curry", @"Instant Noodle with Egg", @"Noodle with BBQ Pork", @"Japanese Noodle with Pork", @"Green Tea", @"Thai Shrimp Cake", @"Angry Birds Cake", @"Ham and Cheese Panini", nil];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [test count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *simpleTableIdentifier = @"SimpleTableCell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+    }
+    
+    cell.textLabel.text = [test objectAtIndex:indexPath.row];
+    return cell;
 }
 
 
