eba1e1b307f53e1143f5232151d891e6839c2434
Leonn <leonn.paiva@gmail.com>
2014-07-01 18:05:50 -0300

    remove trash code

--- EasyLaunches/EasyLaunches/ELImageProcessing.h
+++ EasyLaunches/EasyLaunches/ELImageProcessing.h
@@ -2,12 +2,6 @@
 
 using namespace cv;
 
-const int train_samples = 1;
-const int classes = 10;
-const int sizex = 20;
-const int sizey = 30;
-const int ImageSize = sizex * sizey;
-
 @interface ELImageProcessing :UIImage
 
     + (Mat)cvMatFromUIImage:(UIImage *)image;
