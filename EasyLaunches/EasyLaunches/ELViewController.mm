7fcb2682a0ebbccd0f583616e969b2b371f08415
Leonn <leonn.paiva@gmail.com>
2014-07-16 17:50:18 -0300

    inver ok <> add campo

--- EasyLaunches/EasyLaunches/ELViewController.mm
+++ EasyLaunches/EasyLaunches/ELViewController.mm
@@ -305,7 +305,7 @@ UIButton *button;
                                                     message:message
                                                    delegate:self
                                           cancelButtonTitle:@"Tentar Novamente"
-                                          otherButtonTitles:@"Add campo", @"Ok", nil];
+                                          otherButtonTitles:@"Ok",@"Add campo", nil];
     [toast show];
     
 //    int duration = 1; // duration in seconds
